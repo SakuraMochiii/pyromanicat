b8e143a18f4afb2a23e0f7602eccd6bfe8d48ced
Raine Koizumi <rainekoizumi@berkeley.edu>
2023-09-30 22:06:18 -0700

    added fire bar logic. also fixed warning on empty transparent cup sprite.

--- Assets/Scripts/FireBar.cs
+++ Assets/Scripts/FireBar.cs
@@ -5,18 +5,31 @@ using UnityEngine.UI;
 
 public class FireBar : MonoBehaviour
 {
-    public float minHeight, maxHeight;
+    private float minHeight, maxHeight, currHeight, t;
     private SpawnManager spawnManager;
-    
+    private RectTransform rectTransform;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         spawnManager = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManager>();
+        rectTransform = GetComponent<RectTransform>();
+        minHeight = rectTransform.offsetMin.y;
+        maxHeight = rectTransform.offsetMax.y;
+        currHeight = 0;
+        t = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        t += ((spawnManager.fireCount + 0.0f)/spawnManager.totalObjects - 0.33f) * Time.deltaTime * 0.2f;
+        if (t < 0) t = 0;
+        if (t > 1) t = 1;
+        currHeight = minHeight + (maxHeight - minHeight) * t;
+        rectTransform.offsetMax = new Vector2(rectTransform.offsetMax.x, currHeight) ;
+
     }
 }
