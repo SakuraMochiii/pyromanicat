ef3428a402b4781c6fad0f4226db01d8db7aa223
Tiana Zhou <tianazhou@gmail.com>
2023-09-30 23:22:58 -0700

    tiana changes

--- Assets/Scripts/FireBar.cs
+++ Assets/Scripts/FireBar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class FireBar : MonoBehaviour
@@ -8,8 +9,20 @@ public class FireBar : MonoBehaviour
     private float minHeight, maxHeight, currHeight, t;
     private SpawnManager spawnManager;
     private RectTransform rectTransform;
+    private GameOverScreen gameOverWin;
+    private GameOverScreenLose gameOverLose;
 
+    private void GameOverWin()
+    {
+        SceneManager.LoadScene("End_Win");
+        gameOverWin.Setup(spawnManager.score);
+    }
 
+    private void GameOverLose()
+    {
+        SceneManager.LoadScene("End_Lose");
+        gameOverLose.Setup(spawnManager.score);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -26,8 +39,14 @@ public class FireBar : MonoBehaviour
     void Update()
     {
         t += ((spawnManager.fireCount + 0.0f)/spawnManager.totalObjects - 0.33f) * Time.deltaTime * 0.2f;
+        if (spawnManager.score > 1000) {
+            GameOverWin();
+        }
         if (t < 0) t = 0;
-        if (t > 1) t = 1;
+        if (t > 1)
+        {
+            GameOverLose();
+        }
         currHeight = minHeight + (maxHeight - minHeight) * t;
         rectTransform.offsetMax = new Vector2(rectTransform.offsetMax.x, currHeight) ;
 
---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOverScreen : MonoBehaviour
+{
+    [SerializeField] public TMP_Text totalScore; //in game controller script call gameoverscreen setup with score input
+
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+        totalScore.text = score.ToString() + "points";
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
